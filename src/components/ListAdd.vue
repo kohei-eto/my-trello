3c6725260dfc4ea0a27fcc698370aa0697d86b34
etokohei <kohei.eto@rakus-partners.co.jp>
2020-11-18 18:31:18 +0900

    ListAddコンポーネントを追加

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,27 @@
+<template>
+  <form class="addList" @submit.prevent="addList">
+    <input type="text"
+           v-model="title"
+           class="text-input"
+           placeholder="Add new list">
+    <button type="submit" class="add-button">
+      add
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: ''
+    }
+  },
+  methods: { 
+    addList: function () {
+    this.$store.dispatch('addlist', { title: this.title } )
+    this.title = ''
+  }
+ }
+}
+</script>
